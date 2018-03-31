20afa30c574e9c3b288b68cca7d16d3d8233f62e
4rzael <maxime.agor23@gmail.com>
2018-03-31 04:15:04 +0100

    CSS fixed by the allmighty @SRJanel

--- src/components/NotificationsList.vue
+++ src/components/NotificationsList.vue
@@ -6,11 +6,11 @@
       <b class="caret"></b>
       <span v-if="unreadNumber > 0" class="notification">{{unreadNumber}}</span>
     </template>
-    <div v-for="notif in notifs" :key="notif._id" :class="{ read: notif.read, 'dropdown-item': true }" >
+    <div v-for="notif in notifs" :key="notif._id" :class="{ read: notif.read, 'dropdown-item': true }" style="padding-bottom: 25px;">
       <!-- <a style="float:left" href="#" :class="{ read: notif.read }" @click="readNotification(notif)">{{notif.message}}</a> -->
       <!-- <a style="float:right"><i :class="{ read: notif.read, 'nc-icon': true, 'nc-simple-remove': true }" @click="deleteNotification(notif)"></i></a> -->
       <a style="float:left" href="#" @click="readNotification(notif)">{{notif.message}}</a>
-      <a style="float:right"><i class="nc-icon nc-simple-remove" @click="deleteNotification(notif)"></i></a>
+      <a style="float:right"><i style="cursor:pointer;" class="nc-icon nc-simple-remove" @click="deleteNotification(notif)"></i></a>
     </div>
   </drop-down>
 </template>
