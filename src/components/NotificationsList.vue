cffd745655986a825f34eb3a89ee7545c7663344
4rzael <maxime.agor23@gmail.com>
2018-04-03 04:20:00 +0100

    Improved CSS of notification list

--- src/components/NotificationsList.vue
+++ src/components/NotificationsList.vue
@@ -6,16 +6,20 @@
       <b class="caret"></b>
       <span v-if="unreadNumber > 0" class="notification">{{unreadNumber}}</span>
     </template>
-    <div v-for="notif in notifs" :key="notif._id" :class="{ read: notif.read, 'dropdown-item': true }" style="padding-bottom: 25px;">
+    <div v-for="notif in notifs" :key="notif._id" :class="{'notif-body': true, read: notif.read}">
       <!-- <a style="float:left" href="#" :class="{ read: notif.read }" @click="readNotification(notif)">{{notif.message}}</a> -->
       <!-- <a style="float:right"><i :class="{ read: notif.read, 'nc-icon': true, 'nc-simple-remove': true }" @click="deleteNotification(notif)"></i></a> -->
-      <a style="float:left" href="#" @click="readNotification(notif)">{{notif.message}}</a>
-      <a style="float:right"><i style="cursor:pointer;" class="nc-icon nc-simple-remove" @click="deleteNotification(notif)"></i></a>
+      <a class="notif-text" href="/notifications" @click="readNotification(notif)">{{notif.message}}</a>
+      <a class="notif-x"><i style="cursor:pointer;" class="nc-icon nc-simple-remove" @click="deleteNotification(notif)"></i></a>
+      <span class="notif-date">{{notifDate(notif)}}</span>
+      <a class="notif-read" href="#" @click="readNotification(notif)">read</a>
     </div>
   </drop-down>
 </template>
 
 <script>
+  import moment from 'moment'
+
   export default {
     computed: {
       notifs () {
@@ -31,6 +35,9 @@
       },
       deleteNotification (notif) {
         this.$store.dispatch('deleteNotification', notif)
+      },
+      notifDate (notif) {
+        return moment(notif.date).fromNow()
       }
     },
     mounted () {
@@ -43,4 +50,33 @@
 .read {
   background-color: #d6d6d6;
 }
+
+.notif-body {
+  padding-bottom: 50px;
+}
+
+.notif-text {
+  float:left;
+  width: 90%;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: pre;
+}
+
+.notif-x {
+  float:right;
+  width: 5%;
+}
+
+.notif-date {
+  font-size: 11px;
+  color: grey;
+  float: left;
+}
+
+.notif-read {
+  float: right;
+  width: 25%
+}
+
 </style>
