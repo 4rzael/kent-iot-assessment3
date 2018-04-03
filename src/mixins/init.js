d704e0b263572a9192cea6ec1c58e109c0395074
4rzael <maxime.agor23@gmail.com>
2018-04-03 08:24:52 +0100

    Added autorefresh of all datas every 10 minutes

--- src/mixins/init.js
+++ src/mixins/init.js
@@ -2,10 +2,47 @@ import * as measurementTypes from '../utils/measurementTypes'
 import * as measurementRates from '../utils/measurementRates'
 import * as types from '../store/mutation-types'
 
-export const initMixin = {
-   mounted: async function () {
-    this.$store.dispatch('retrieveSites')
+import { CLEAR_KEY } from '../store/plugins/cache'
 
+export const initMixin = {
+  methods: {
+    clearCache: function () {
+      this.$store.commit(CLEAR_KEY, 'retrieveSites')
+      this.$store.commit(CLEAR_KEY, 'retrieveDevices')
+      this.$store.commit(CLEAR_KEY, 'retrieveMeasurements')
+      this.$store.commit(CLEAR_KEY, 'retrieveLastMeasurement')
+    },
+    loadEverything: async function () {
+      // get devices and stores
+      await Promise.all([
+        this.$store.dispatch('retrieveSites'),
+        this.$store.dispatch('retrieveDevices')
+      ])
+
+      // then all measurements and battery levels
+      await Promise.all(
+        Object.values(this.$store.state.api.preciseDevices)
+        .map(device => Promise.all(
+          // get measurements
+          device.measures
+          .filter(m => m !== measurementTypes.MEASUREMENT_BATTERY)
+          .map(m => this.$store.dispatch('retrieveMeasurements', {
+            deviceId: device.id,
+            measurementType: m,
+            measurementRate: measurementRates.MEASUREMENT_HOURS
+          }))
+          .concat(
+            // get battery levels
+            this.$store.dispatch('retrieveLastMeasurement', {
+              deviceId: device.id,
+              measurementType: measurementTypes.MEASUREMENT_BATTERY
+            })
+          ))
+        )
+      )
+    }
+  },
+  mounted: async function () {
     // handle toasting the notifications
     this.$store.subscribe(({type, payload}) => {
       if (type === types.ADD_NOTIFICATION) {
@@ -14,33 +51,13 @@ export const initMixin = {
       }
     })
 
-    // get devices and stores
-    await Promise.all([
-      this.$store.dispatch('retrieveSites'),
-      this.$store.dispatch('retrieveDevices')
-    ])
+    this.loadEverything()
 
-    // then all measurements and battery levels
-    await Promise.all(
-      Object.values(this.$store.state.api.preciseDevices)
-      .map(device => Promise.all(
-        // get measurements
-        device.measures
-        .filter(m => m !== measurementTypes.MEASUREMENT_BATTERY)
-        .map(m => this.$store.dispatch('retrieveMeasurements', {
-          deviceId: device.id,
-          measurementType: m,
-          measurementRate: measurementRates.MEASUREMENT_HOURS
-        }))
-        .concat(
-          // get battery levels
-          this.$store.dispatch('retrieveLastMeasurement', {
-            deviceId: device.id,
-            measurementType: measurementTypes.MEASUREMENT_BATTERY
-          })
-        ))
-      )
-    )
+    // every 10 minutes, reload data
+    setInterval(() => {
+      this.clearCache()
+      this.loadEverything()
+    }, 1000 * 60 * 10)
   }
 }
 
