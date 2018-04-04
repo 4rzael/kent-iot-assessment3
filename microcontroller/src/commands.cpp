29b1f7bfec2936c28e0fd1c760264530e6c659a7
SRJanel <nicolas.giannelos@epitech.eu>
2018-04-04 20:11:37 +0100

    Added mesg_counter to microcontroller

--- microcontroller/src/commands.cpp
+++ microcontroller/src/commands.cpp
@@ -1,5 +1,8 @@
+#include <sstream>
 #include "Commands.hh"
 #include "Network.hh"
+
+unsigned long long   g_mesg_counter = 0;
 
 /*
 ** An array to be able to loop through functions once they are created
@@ -12,12 +15,14 @@ t_commands_ctrl     g_commands_ctrl[LIMIT] = {
 };
 
 # define ENTRY(UNUSED1, MESG, LED_COLOR, FUNCTION) void FUNCTION(const char * const house) { \
+    std::stringstream           ss;                                     \
     controlLED(LED_COLOR);                                              \
     display_screen("[*] " MESG, house);                                 \
     fprintf(stdout, "[*] %s@%s\n", MESG, house);                        \
     wait(RANDOM_NBR_RANGE(TASK_TIME_MIN, TASK_TIME_MAX));               \
     display_screen(MESG_LAST_ACTION MESG, house);                       \
-    send_mesg(MESG_MICROC_FINISH MESG);                                 \
+    ss << MESG_MICROC_FINISH << MESG << "::" << ++g_mesg_counter;       \
+    send_mesg(ss.str().c_str());                                        \
     controlLED(off);                                                    \
   }
     COMMANDS_CTRL_TABLE
